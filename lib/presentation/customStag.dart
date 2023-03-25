5e24d13bf3f1613f96e705dd5b90f1432578635c
amarjeetjeet <kush.amarjeet22@gmail.com>
2023-03-25 23:32:23 +0530

    "Creating custom list with multiple list"

--- lib/presentation/customStag.dart
+++ lib/presentation/customStag.dart
@@ -2,13 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:shigoto/presentation/people_card/people_card.dart';
 
-class CustomStag extends StatelessWidget {
+class CustomStag extends StatefulWidget {
   const CustomStag({super.key});
+
+  @override
+  State<CustomStag> createState() => _CustomStagState();
+}
+
+class _CustomStagState extends State<CustomStag> {
+
+  List<String> firstList = List.generate(100, (index) => 'Item ${index + 1}');
+
+  List<String> secondList = List.generate(8, (index) => 'Second item ${index}');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      body: ListView.builder(
+        itemCount: firstList.length,
+        itemBuilder: (BuildContext context, int index) {
+          if ((index) % 10 == 0) { // Check if the index is a multiple of 10
+            int secondListIndex = ((index + 1) ~/ 10) % secondList.length;
+            // Calculate the index of the item in the second list using integer division and modulo
+            return ListTile(
+              title: Text(secondList[secondListIndex]), // Print the item from the second list
+            );
+          } else {
+            return ListTile(
+              title: Text(firstList[index]), // Print the item from the first list
+            );
+          }
+        },
+      )
     );
   }
 }
