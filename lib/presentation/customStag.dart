cc5104450016430697f732f1d41f821d2ad6a082
amarjeetjeet <kush.amarjeet22@gmail.com>
2023-03-25 23:37:07 +0530

    "Creating custom list with multiple list"

--- lib/presentation/customStag.dart
+++ lib/presentation/customStag.dart
@@ -10,30 +10,31 @@ class CustomStag extends StatefulWidget {
 }
 
 class _CustomStagState extends State<CustomStag> {
-
   List<String> firstList = List.generate(100, (index) => 'Item ${index + 1}');
 
-  List<String> secondList = List.generate(8, (index) => 'Second item ${index}');
+  List<String> secondList = List.generate(10, (index) => 'Second item ${index}');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: firstList.length,
-        itemBuilder: (BuildContext context, int index) {
-          if ((index) % 10 == 0) { // Check if the index is a multiple of 10
-            int secondListIndex = ((index + 1) ~/ 10) % secondList.length;
-            // Calculate the index of the item in the second list using integer division and modulo
+      body: ListView.separated(
+          itemBuilder: (c, index) {
             return ListTile(
-              title: Text(secondList[secondListIndex]), // Print the item from the second list
+              title:
+                  Text(firstList[index]), // Print the item from the first list
             );
-          } else {
-            return ListTile(
-              title: Text(firstList[index]), // Print the item from the first list
-            );
-          }
-        },
-      )
+          },
+          separatorBuilder: (c, index) {
+            if ((index + 1) % 10 == 0) {
+              int secondListIndex = ((index + 1) ~/ 10) % secondList.length;
+              return ListTile(
+                title: Text(secondList[secondListIndex]), // Print the item from the second list
+              );
+            } else {
+              return Container();
+            }
+          },
+          itemCount: firstList.length),
     );
   }
 }
